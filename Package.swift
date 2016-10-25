bb9b941d2f16db5f3eb3ae6627038fb24cd7035e
Ubaldo Cotta <ucotta@me.com>
2016-10-25 18:02:45 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
 	targets: [],
 	dependencies: {
         .Package(url: "https://github.com/ucotta/MySQL-ConnectionPool.git"), majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git"), majorVersion: 2, minor: 0)
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git"), majorVersion: 2, minor: 0),
 	exclude: []
 )
 
