b0c3babe51740bf2dd9eb0f2e770d0e8e74b6b17
Ubaldo Cotta <ucotta@me.com>
2016-10-25 19:27:28 +0300

    include mySQL

--- Package.swift
+++ Package.swift
@@ -5,7 +5,13 @@ let package = Package(
 	targets: [],
 	dependencies: [
 	        .Package(url: "https://github.com/ucotta/MySQL-ConnectionPool.git", majorVersion: 0, minor: 20),
-        	.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 0)
+        	.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 0),
+		#if os(OSX)
+        	.Package(url: "https://github.com/PerfectlySoft/Perfect-mysqlclient.git", majorVersion: 2, minor: 0)
+		#else
+        	.Package(url: "https://github.com/PerfectlySoft/Perfect-mysqlclient-Linux.git", majorVersion: 2, minor: 0)
+		#endif		
 	],
 	exclude: []
 )
+
