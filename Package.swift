425eef2283739a5bbe76ddbfacf6e5373a600f1c
Ubaldo Cotta <ucotta@me.com>
2016-10-25 19:33:42 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,23 @@
-import PackageDescription
-
-let package = Package(
-	name: "SessionMySQL",
-	targets: [],
-	dependencies: [
-	        .Package(url: "https://github.com/ucotta/MySQL-ConnectionPool.git", majorVersion: 0, minor: 20),
-        	.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 0),
 #if os(OSX)
-        	.Package(url: "https://github.com/PerfectlySoft/Perfect-mysqlclient.git", majorVersion: 2, minor: 0)
-#else
-        	.Package(url: "https://github.com/PerfectlySoft/Perfect-mysqlclient-Linux.git", majorVersion: 2, minor: 0)
-#endif		
-	],
-	exclude: []
+let package = Package(
+     name: "SessionMySQL",
+     targets: [],
+     dependencies: [
+          .Package(url: "https://github.com/ucotta/MySQL-ConnectionPool.git", majorVersion: 0, minor: 20),
+          .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 0),
+          .Package(url: "https://github.com/PerfectlySoft/Perfect-mysqlclient.git", majorVersion: 2, minor: 0)
+     ],
+     exclude: []
 )
-
+#else
+let package = Package(
+     name: "SessionMySQL",
+     targets: [],
+     dependencies: [
+          .Package(url: "https://github.com/ucotta/MySQL-ConnectionPool.git", majorVersion: 0, minor: 20),
+          .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 0),
+          .Package(url: "https://github.com/PerfectlySoft/Perfect-mysqlclient-Linux.git", majorVersion: 2, minor: 0)
+     ],
+     exclude: []
+)
+#endif
