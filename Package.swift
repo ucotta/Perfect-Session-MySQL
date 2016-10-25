9ae53660554fde43c22e47f6b442a30f124640cf
Ubaldo Cotta <ucotta@me.com>
2016-10-25 23:54:31 +0300

    .

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
 import PackageDescription
 
-#if os(OSX)
 let package = Package(
      name: "SessionMySQL",
      targets: [],
@@ -11,15 +10,3 @@ let package = Package(
      ],
      exclude: []
 )
-#else
-let package = Package(
-     name: "SessionMySQL",
-     targets: [],
-     dependencies: [
-          .Package(url: "https://github.com/ucotta/MySQL-ConnectionPool.git", majorVersion: 0, minor: 20),
-          .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 0),
-          .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 0),
-     ],
-     exclude: []
-)
-#endif
