23f907a80f973b1a7e71b15b9f361ab0089c8c3e
Ubaldo Cotta <ucotta@me.com>
2016-11-09 18:15:54 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
      name: "SessionMySQL",
      targets: [],
      dependencies: [
-          .Package(url: "https://github.com/ucotta/MySQL-ConnectionPool.git", majorVersion: 0, minor: 20),
+          .Package(url: "https://github.com/ucotta/MySQL-ConnectionPool.git", majorVersion: 0),
           .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 0),
           .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 0),
      ],
