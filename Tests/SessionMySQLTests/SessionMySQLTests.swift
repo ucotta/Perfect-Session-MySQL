d6487df4ad3a3cc7240c9fcd35dca972da01120e
Ubaldo Cotta <ucotta@me.com>
2016-11-11 01:27:25 +0200

    repacked

---
+++ Tests/SessionMySQLTests/SessionMySQLTests.swift
@@ -0,0 +1,23 @@
+//
+//  BrilliantTemplateTests.swift
+//  BrilliantTemplateTests
+//
+//  Created by Ubaldo Cotta on 7/11/16.
+//
+//
+
+import XCTest
+@testable import SessionMySQL
+
+class SessionMySQLTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
